24e033ba38366c2f666ff90566ebf6d41d8c7e6f
Jorge Kato <katojorge@hotmail.com>
2018-04-01 17:16:09 -0300

    Criado classe tipo preco.

---
+++ src/main/java/br/com/casadocodigo/models/TipoPreco.java
@@ -0,0 +1,26 @@
+/*
+ * TODO Produto $(product_name} - ${product_description}<br>
+ *
+ * Data de Criação: 31 de mar de 2018<br>
+ * <br>
+ * Todos os direitos reservados.
+ */
+
+package br.com.casadocodigo.models;
+
+
+/** 
+ * DOCUMENTAÇÃO DA CLASSE <br>
+ * ---------------------- <br>
+ * FINALIDADE: <br>
+ * TODO Definir documentação da classe. <br>
+ * <br>
+ * HISTÓRICO DE DESENVOLVIMENTO: <br>
+ * 31 de mar de 2018 - @author jorge - Primeira versão da classe. <br>
+ *<br>
+ *<br>
+ * LISTA DE CLASSES INTERNAS: <br>
+ */
+public enum TipoPreco {
+    EBOOK, IMPRESSO, COMBO;
+}
