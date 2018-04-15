2e231ab047a35f6b736931a12ee1e4289fb0a8f0
Jorge Kato <katojorge@hotmail.com>
2018-04-15 10:23:49 -0300

    Criado método para remover item do carrinho e redirecinar para view de itens.

--- src/main/java/br/com/casadocodigo/controller/CarrinhoComprasController.java
+++ src/main/java/br/com/casadocodigo/controller/CarrinhoComprasController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -48,18 +49,53 @@ public class CarrinhoComprasController {
     @Autowired
     private ProdutoDAO produtoDAO;
 
+    /**
+     * Método que adiciona um item ao carrinho de compras
+     * @param produtoId
+     * @param tipoPreco
+     * @return
+     */
     @RequestMapping("/add")
     public ModelAndView add ( Integer produtoId , TipoPreco tipoPreco ) {
-        ModelAndView modelAndView = new ModelAndView( "redirect:/produtos" );
+        ModelAndView modelAndView = new ModelAndView( "redirect:/carrinho" );
         CarrinhoItem carrinhoItem = criaItem( produtoId , tipoPreco );
         carrinho.add(carrinhoItem);
         return modelAndView;
     }
 
+    /**
+     * Método que cria um item para ser adicionado ao carrinho de compras
+     * @param produtoId
+     * @param tipoPreco
+     * @return
+     */
     public CarrinhoItem criaItem ( Integer produtoId , TipoPreco tipoPreco ) {
         Produto produto = produtoDAO.find( produtoId );
         CarrinhoItem carrinhoItem = new CarrinhoItem( produto , tipoPreco );
         
         return carrinhoItem;
     }
+    
+    /**
+     * Método que redireciona para a view dos itens do carrinho de compras.
+     * @return
+     */
+    @RequestMapping(method=RequestMethod.GET)
+    public ModelAndView itens() {
+        return new ModelAndView("/carrinho/itens");
+    }
+    
+    /**
+     * Método que remmove um item do carrinho de compras.
+     * @param produtoId
+     * @param tipoPreco
+     * @return
+     */
+    @RequestMapping("/remover")
+    public ModelAndView remover(Integer produtoId, TipoPreco tipoPreco) {
+       carrinho.remover(produtoId, tipoPreco);
+        
+        return new ModelAndView("redirect:/carrinho");
+    }
+    
 }
