e31c1e51e82da248ffe98979a0704aa7b380e98e
Jorge Kato <katojorge@hotmail.com>
2018-04-25 20:25:32 -0300

    Habilita o servlet padrão para utilizar o css e js.

--- src/main/java/br/com/casadocodigo/conf/AppWebConfiguration.java
+++ src/main/java/br/com/casadocodigo/conf/AppWebConfiguration.java
@@ -21,7 +21,9 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -54,7 +56,7 @@ import br.com.casadocodigo.models.CarrinhoCompras;
 @EnableWebMvc
 @ComponentScan(basePackageClasses = { HomeController.class, ProdutoDAO.class, FileSaver.class, CarrinhoCompras.class })
 @EnableCaching
-public class AppWebConfiguration {
+public class AppWebConfiguration extends WebMvcConfigurerAdapter{
 
 	/**
 	 * Método que ajuda o Spring a encontrar as views, definindo o caminho e a
@@ -161,6 +163,11 @@ public class AppWebConfiguration {
 		resolver.setContentNegotiationManager(manager);
 
 		return resolver;
+	}
+	
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
 	}
 
 }
