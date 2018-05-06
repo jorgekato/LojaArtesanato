26f7f0104dcf85849e741c2558b5f1f3802b820c
Jorge Kato <katojorge@hotmail.com>
2018-05-06 20:51:58 -0300

    Criado classe de configuração para utilização do filtro de segurança.

---
+++ src/main/java/br/com/casadocodigo/conf/SpringSecurityFilterConfiguration.java
@@ -0,0 +1,21 @@
+package br.com.casadocodigo.conf;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * 
+ * DOCUMENTAÇÃO DA CLASSE <br>
+ * ---------------------- <br>
+ * FINALIDADE: <br>
+ * Classe de configuração responsável por inicializar o filtro de segurança.
+ * <br>
+ * HISTÓRICO DE DESENVOLVIMENTO: <br>
+ * 27 de abr de 2018 - @author jorge - Primeira versão da classe. <br>
+ *<br>
+ *<br>
+ * LISTA DE CLASSES INTERNAS: <br>
+ */
+
+public class SpringSecurityFilterConfiguration extends AbstractSecurityWebApplicationInitializer{
+
+}
