9db7f3b45705587cca9d9cdc8696dbc8d8b30073
Jorge Kato <katojorge@hotmail.com>
2018-05-06 21:11:50 -0300

    Refatora classe.

--- src/main/java/br/com/casadocodigo/conf/AppWebConfiguration.java
+++ src/main/java/br/com/casadocodigo/conf/AppWebConfiguration.java
@@ -21,9 +21,7 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -56,7 +54,7 @@ import br.com.casadocodigo.models.CarrinhoCompras;
 @EnableWebMvc
 @ComponentScan(basePackageClasses = { HomeController.class, ProdutoDAO.class, FileSaver.class, CarrinhoCompras.class })
 @EnableCaching
-public class AppWebConfiguration extends WebMvcConfigurerAdapter{
+public class AppWebConfiguration {
 
 	/**
 	 * Método que ajuda o Spring a encontrar as views, definindo o caminho e a
@@ -163,17 +161,6 @@ public class AppWebConfiguration extends WebMvcConfigurerAdapter{
 		resolver.setContentNegotiationManager(manager);
 
 		return resolver;
-	}
-	
-	/**
-	 * 
-	 * Método que configura o servlet padrão para que atenda as requisições de arquivos com css e js
-	 * @param configurer - habilita o servlet padrão.
-	 * @see org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter#configureDefaultServletHandling(org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer)
-	 */
-	@Override
-	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-		configurer.enable();
 	}
 
 }
