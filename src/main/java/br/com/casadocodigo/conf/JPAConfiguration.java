5d62bfb44f5b606c1d4eb6f5338eb5aa3deafda8
Jorge Kato <katojorge@hotmail.com>
2018-09-08 17:14:17 -0300

    aplicationTests Refatoração da classe para utilização dos profiles de dev e test.

--- src/main/java/br/com/casadocodigo/conf/JPAConfiguration.java
+++ src/main/java/br/com/casadocodigo/conf/JPAConfiguration.java
@@ -10,8 +10,10 @@ package br.com.casadocodigo.conf;
 import java.util.Properties;
 
 import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
@@ -37,33 +39,53 @@ public class JPAConfiguration {
     /**
      * 
      * Método responsável por configurar a base de dados a ser utilizado.
+     * @param dataSource 
      * @return
      */
     @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean () {
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean (DataSource dataSource) {
         LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
         JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
         
         factoryBean.setJpaVendorAdapter( vendorAdapter );
         
+        factoryBean.setDataSource( dataSource );
+        
+        factoryBean.setJpaProperties( props() );
+        
+        factoryBean.setPackagesToScan( "br.com.casadocodigo.models" );
+        
+        return factoryBean;
+    }
+
+
+    /** 
+     * Método que informa as propriedades de configuração do hibernate
+     * @return
+     */
+    @Bean
+    private Properties props () {
+        Properties props = new Properties();
+        props.setProperty( "hibernate.dialect" , "org.hibernate.dialect.MySQL5InnoDBDialect" );
+        props.setProperty( "hibernate.show_sql" , "true" );
+        props.setProperty( "hibernate.hbm2ddl.auto" , "update" );
+        return props;
+    }
+
+
+    /** 
+     * TODO Descrição do Método
+     * @return
+     */
+    @Bean
+    @Profile("dev")
+    private DataSource dataSource () {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setUsername( "root" );
         dataSource.setPassword( "" );
         dataSource.setUrl( "jdbc:mysql://localhost:3306/casadocodigo" );
         dataSource.setDriverClassName( "com.mysql.jdbc.Driver" );
-        
-        factoryBean.setDataSource( dataSource );
-        
-        Properties props = new Properties();
-        props.setProperty( "hibernate.dialect" , "org.hibernate.dialect.MySQL5InnoDBDialect" );
-        props.setProperty( "hibernate.show_sql" , "true" );
-        props.setProperty( "hibernate.hbm2ddl.auto" , "update" );
-        
-        factoryBean.setJpaProperties( props );
-        
-        factoryBean.setPackagesToScan( "br.com.casadocodigo.models" );
-        
-        return factoryBean;
+        return dataSource;
     }
     
     
