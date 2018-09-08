51833068c05667bad7952e5280494ba9f398862f
Jorge Kato <katojorge@hotmail.com>
2018-09-08 17:15:54 -0300

    aplicationTests Criado classe de conf para utilização de profile de test para utilizar banco de teste.

---
+++ src/test/java/br/com/casadocodigo/loja/conf/DataSourceConfigurationTest.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package br.com.casadocodigo.loja.conf;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+/** 
+ * DOCUMENTAÇÃO DA CLASSE <br>
+ * ---------------------- <br>
+ * FINALIDADE: <br>
+ * TODO Definir documentação da classe. <br>
+ * <br>
+ * HISTÓRICO DE DESENVOLVIMENTO: <br>
+ * 8 de set de 2018 - @author jorge - Primeira versão da classe. <br>
+ *<br>
+ *<br>
+ * LISTA DE CLASSES INTERNAS: <br>
+ */
+
+
+public class DataSourceConfigurationTest {
+
+    @Bean
+    @Profile("test")
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl( "jdbc:mysql://localhost:3306/casadocodigo_test" );
+        dataSource.setDriverClassName( "com.mysql.jdbc.Driver" );
+        dataSource.setUsername( "root" );
+        dataSource.setPassword( "" );
+        
+        return dataSource;
+    }
+}
